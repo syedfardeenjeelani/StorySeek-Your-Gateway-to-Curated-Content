667c8d0fe6c0c7d9dace7af3ca75b1517f2373e5
Fardeen <154688163+syedfardeenjeelani@users.noreply.github.com>
2024-06-20 11:42:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Cards from "./components/Cards";
 import Search from "./components/Search";
-import Context from "./utils/Context";
+
 
 function App() {
   return <>
-  <Context/>
+
   <Search/>
   <Cards/>
   </>;
